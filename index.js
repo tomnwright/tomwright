738732671c61225e949eb4979f9af53a060f4855
Tom Wright <tomnwright@outlook.com>
2025-06-03 08:36:35 +0100

    Basic UI element structure

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-import { GridMaster } from './lib/grid.js';
-// import { GoLGrid } from './lib/gol.js';
+import { TextGrid, GridMaster } from "./lib/grid.js";
+import { UIGrid, Container } from "./lib/ui.js";
+import { GoLGrid } from "./lib/gol.js";
 
-console.log("Hey");
-
-const calc_div = document.getElementById('ui-grid');
-const ui_div = document.getElementById('ui-grid');
-const gol_div = document.getElementById('gol-grid');
-
-
-
+const master_div = document.getElementById("master-grid");
+const ui_div = document.getElementById("ui-grid");
+// const gol_div = document.getElementById("gol-grid");
 
 // set up gol grid object
+// const golGrid = new TextGrid(gol_div);
 // const gol = new GoLGrid();
-//  - handles GoL grid
 
 // set up ui grid object
-// const ui = new UIGrid();
-
-// const children = [gol, ui];
+const uiGrid = new UIGrid(
+  ui_div,
+  Container([
+    Container(["HI there"]),
+    "Hello world,this is a big old test",
+    "Hello world,this is a big old test",
+  ])
+);
+// const uiGrid = new TextGrid(ui_div);
+// uiGrid.domObj.textContent = "Hey"
 
 // create the grid controlller
-const grid = new GridMaster(calc_div, []);
+const grid = new GridMaster(master_div, [uiGrid], {
+  respace: true,
+  default_fontSize: 40,
+  min_spacing: { row: -8, col: 2 },
+});
+
+console.log("Fitting...");
+grid.fitToWindow();
+
 // const grid = new GridMaster(children, font-size, min-fontSize, min-rows, min-cols);
 //  - fits a grid to the viewport
 //  - determines grid dimensions and formatting
-//  - handles cursor to 
+//  - handles cursor to
 //  - passes info to children
-
-
 
 // start GoL animation
 // gol.start_animation();
+
+// when to draw UI...
+// - upon initialisation
+// -
